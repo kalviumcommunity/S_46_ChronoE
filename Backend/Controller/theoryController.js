319b67cb9e41a45689062b917af98adb0eeb7ad6
AlexCFrost <siva.ganesh.c18@gamil.com>
2024-02-13 10:06:04 +0530

    created routes and connected Db.

---
+++ Backend/Controller/theoryController.js
@@ -0,0 +1,72 @@
+const Theorie = require('../model/theories')
+const mongoose = require('mongoose')
+
+//Get all theories
+const getTheories = async(req, res)=>{
+    const theories = await Theorie.find({}).sort({createdAt: -1})
+
+
+    res.status(200).json(theories)
+}
+
+
+//Create a new theory
+const createTheory = async (req, res)=>{
+    const {theoryDetails} = req.body
+
+    try{
+        const theorie = await Theorie.create({theoryDetails})
+        res.status(200).json(theorie)
+    } catch (error){
+        res.status(400).json({error: error.message})
+    }
+
+    res.json({messs: "post request accepted"})
+}
+
+// Delete theorie
+
+const deleteTheory = async(req, res)=>{
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: "No such Theory"})
+    }
+
+    const theorie = await Theorie.findOneAndDelete({_id: id}) 
+    if (!theorie){
+        return res.status(400).json({error: 'No such Theory'})
+    }
+
+    res.status(200).json(theorie)
+}
+
+// Update theory
+
+const updateTheory = async (req, res)=>{
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: "No such Theory"})
+    }
+
+    const theorie = await Theorie.findByIdAndUpdate({_id: id},{
+        ...req.body
+    })
+   
+    if (!theorie){
+        return res.status(400).json({error: 'No such Theory'})
+    }
+
+    res.status(200).json(theorie)
+
+}
+
+
+// Exporting 
+module.exports = {
+    createTheory,
+    getTheories,
+    deleteTheory,
+    updateTheory
+}
