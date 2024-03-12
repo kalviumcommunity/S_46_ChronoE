ac924ba605a292e19527a9cd5baa5712c2a0aa92
AlexCFrost <siva.ganesh.c18@gamil.com>
2024-03-12 10:21:01 +0530

    Changed host with render

--- Frontend/ChronoE/src/slides/UpdateTheory.jsx
+++ Frontend/ChronoE/src/slides/UpdateTheory.jsx
@@ -11,7 +11,7 @@ function UpdateTheory() {
     const { user } = useAuthContext();
 
     useEffect(() => {
-        axios.get(`http://localhost:3000/api/theories/${id}`)
+        axios.get(`https://chronoe-time.onrender.com/api/theories/${id}`)
             .then((result) => {
                 console.log(result);
                 setTheory(result.data.theoryDetails);
@@ -32,7 +32,7 @@ function UpdateTheory() {
             }
         };
 
-        axios.put(`http://localhost:3000/api/theories/${id}`, { theoryDetails: theory }, config)
+        axios.put(`https://chronoe-time.onrender.com0/api/theories/${id}`, { theoryDetails: theory }, config)
             .then((result) => {
                 console.log(result);
                 navigate('/');
